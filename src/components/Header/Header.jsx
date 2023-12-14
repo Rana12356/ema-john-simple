bc0fa9116ede1e25139e02e783069c55f6113361
Rana12356 <rana15-12356@diu.edu.bd>
2023-12-14 12:56:02 +0600

    navigation part finished and products added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/manage-inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
