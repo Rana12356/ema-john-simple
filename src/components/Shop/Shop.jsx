fee964ca66dd0e6ddbe0314f1926f642f8fcccd3
Rana12356 <rana15-12356@diu.edu.bd>
2023-12-19 10:37:06 +0600

    retrieve product from stored cart and set it to cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -16,10 +17,27 @@ const Shop = () => {
 
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedProduct = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedProduct.push(addedProduct);
+            }
+        }
+        setCart(savedProduct);
+
+    }, [products]);
+
     const handleAddToCart = (product) => {
         // cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+
+        addToDb(product.id);
     }
 
     return (
