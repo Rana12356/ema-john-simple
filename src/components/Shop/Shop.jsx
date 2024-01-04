bc1fd729bb71e5ccbff957e3ebe5f64b21702999
Rana12356 <rana15-12356@diu.edu.bd>
2024-01-04 20:50:57 +0600

    implemented delete from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -28,13 +28,24 @@ const Shop = () => {
                 savedProduct.push(addedProduct);
             }
         }
-        setCart(savedProduct);
+        setCart(savedProduct)
 
     }, [products]);
 
     const handleAddToCart = (product) => {
         // cart.push(product);
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
 
         addToDb(product.id);
